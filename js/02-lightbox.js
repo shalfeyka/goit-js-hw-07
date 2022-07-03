99933fd0b47fe6ac470ca73117cae71fc6647d3f
Marina Checheneva <m.checheneva140201@gmail.com>
2022-07-03 18:27:55 +0300

    cantdotask2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,17 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const imgGallery = document.querySelector('.gallery');
+console.log(imgGallery);
+
+const listImages = galleryItems
+.map(item => `<div class='gallery__item'>
+<a class="gallery__link" href=${item.original}>
+<img class='gallery__image'
+src = ${item.preview}
+alt = ${item.description}/>
+</a>
+</div>`).join('');
+console.log(listImages)
+imgGallery.innerHTML = listImages;
+imgGallery.addEventListener('click', showModal);
